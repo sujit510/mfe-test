ba0af50dca9055f4ca6a80b0fd2bd4fc5a216677
Sujit Singh <sujit510@gmail.com>
2022-08-30 17:34:21 +0530

    Added simple illustration of micro frontend without any federation

---
+++ container/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+    devMiddleware: {
+      index: "index.html",
+      writeToDisk: true, // If this is false, webpack cretes and keeps bundle in memory and runs from there.
+    },
+    liveReload: true,
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve("./index.html"),
+    }),
+  ],
+};
