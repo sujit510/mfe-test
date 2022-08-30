d2d1679b97fbcd059ed7077d0bdfd9e51a6cc79b
Sujit Singh <sujit510@gmail.com>
2022-08-30 17:34:57 +0530

    Added simple illustration of micro frontend without any federation

---
+++ header/src/index.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div style={{ backgroundColor: "yellow", height: "50px", width: "100%" }} />
+  );
+}
+
+const renderFn = (rootEle) => {
+  ReactDOM.render(<App />, rootEle);
+};
+
+window.renderFn = renderFn;
+
+if (!window.containerContext) {
+  renderFn(document.querySelector("#root"));
+}
