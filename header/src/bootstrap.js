51a53031a399756d6991eb5e969ef549239c59c4
Sujit Singh <sujit510@gmail.com>
2022-09-20 13:21:22 +0530

    Twekaed Header to have link to the Github repo

--- header/src/bootstrap.js
+++ header/src/bootstrap.js
@@ -1,20 +1,43 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import GithubLogo from './github-logo.png';
 
 function App() {
-  return (
-    <div style={{ backgroundColor: "yellow", height: "50px", width: "100%" }} />
-  );
+    return (
+        <div
+            style={{
+                backgroundColor: 'yellow',
+                height: '50px',
+                width: '100%',
+                textAlign: 'right',
+                verticalAlign: 'middle',
+            }}
+        >
+            <div style={{ padding: '10px' }}>
+                <a
+                    target={'_blank'}
+                    href="https://github.com/sujit510/mfe-test/"
+                >
+                    <img src={GithubLogo} />
+                </a>
+            </div>
+        </div>
+    );
 }
 
 const renderFn = (rootEle) => {
-  ReactDOM.render(<App />, rootEle);
+    rootEle.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 };
 
 // window.renderFn = renderFn;
 
 if (!window.containerContext) {
-  renderFn(document.querySelector("#root"));
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    renderFn(root);
 }
 
 export { renderFn };
--- header/webpack.config.js
+++ header/webpack.config.js
@@ -1,50 +1,79 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { ModuleFederationPlugin } = require("webpack").container;
-const packageJson = require("./package.json");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const packageJson = require('./package.json');
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: __dirname + "/dist",
-    // publicPath: "/",
-    publicPath: "http://mfe-react-guest.sujitsingh.in/",
-    filename: "bundle.js",
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "./dist"),
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'http://mfe-react-guest.sujitsingh.in/',
+        filename: 'bundle.js',
+        publicPath: '/',
     },
-    devMiddleware: {
-      index: "index.html",
-      writeToDisk: true, // If this is false, webpack cretes and keeps bundle in memory and runs from there.
+    target: 'web',
+    devServer: {
+        static: ['./dist'],
+        // devMiddleware: {
+        //     index: 'index.html',
+        //     // writeToDisk: true, // If this is false, webpack cretes and keeps bundle in memory and runs from there.
+        // },
+        liveReload: true,
+        hot: true,
+        // port: 8083,
+        open: true,
+
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
     },
-    liveReload: true,
-    port: 8081,
-    open: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
+    performance: {
+        hints: false,
+    },
+    resolve: {
+        /** "extensions"
+         * If multiple files share the same name but have different extensions, webpack will
+         * resolve the one with the extension listed first in the array and skip the rest.
+         * This is what enables users to leave off the extension when importing
+         */
+        extensions: ['.js', '.jsx', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(jpg|png|jpeg)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        // This decides if asset is to be merged into bundle js (type: 'asset/inline') itself
+                        // or kept as separate file (type: 'asset/resource')
+                        // We have image of size 11.6 KB so
+                        maxSize: 11.6 * 1024, // Creates inside bundle js (asset/inline)
+                        // maxSize: 11.5 * 1024 // Creates separate resource (asset/resource)
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: path.resolve('./index.html'),
+        }),
+        new ModuleFederationPlugin({
+            name: 'header',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './HeaderComponent': './src/bootstrap',
+            },
+            shared: packageJson.dependencies,
+        }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve("./index.html"),
-    }),
-    new ModuleFederationPlugin({
-      name: "header",
-      filename: "remoteEntry.js",
-      exposes: {
-        "./HeaderComponent": "./src/bootstrap",
-      },
-      shared: packageJson.dependencies,
-    }),
-  ],
 };
