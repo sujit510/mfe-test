874520f881720845329e5adb6c800726a25a19e9
Sujit Singh <sujit510@gmail.com>
2022-09-20 11:41:50 +0530

    Added some info on Host app

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,21 +1,25 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-import { renderFn } from "header/HeaderComponent";
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { renderFn } from 'header/HeaderComponent';
 
 function App() {
-  const ref = React.useRef(null);
-  React.useEffect(() => {
-    // window.renderFn(ref.current);
-    renderFn(ref.current);
-  }, []);
-  return (
-    <>
-      <div ref={ref}></div>
-      <h1>Container (Host App)</h1>
-    </>
-  );
+    const ref = React.useRef(null);
+    React.useEffect(() => {
+        // window.renderFn(ref.current);
+        renderFn(ref.current);
+    }, []);
+    return (
+        <>
+            <div ref={ref}></div>
+            <h1>Welcome to the Micro Frontend Container (Host App)</h1>
+            <h4>
+                This imports exposed Header module function from
+                http://mfe-react-guest.sujitsingh.in/
+            </h4>
+        </>
+    );
 }
 
 window.containerContext = true;
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
