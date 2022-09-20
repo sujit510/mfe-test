8554babab350900d36bdc24f62733ef8896198f4
Sujit Singh <sujit510@gmail.com>
2022-09-20 12:01:27 +0530

    test commit for pipeline

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -13,7 +13,7 @@ function App() {
             <div ref={ref}></div>
             <h1>Welcome to the Micro Frontend Container i.e. Host App</h1>
             <h4>
-                This imports exposed Header module function from
+                This imports exposed Header module function from -
                 http://mfe-react-guest.sujitsingh.in/
             </h4>
         </>
